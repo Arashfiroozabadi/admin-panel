49573096434fcdc1f7bc53edb146d98fb084306b
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-09-02 17:41:19 +0200

    exported new component

--- src/components/themed/index.tsx
+++ src/components/themed/index.tsx
@@ -2,3 +2,5 @@ export { default as MenuButton } from "./MenuBtn";
 export { default as NavBtn } from "./NavBtn";
 export { default as Container } from "./Container";
 export { default as Typography } from "./Typography";
+export { default as IconButton } from "./IconButton";
+export { default as Main } from "./Main";
