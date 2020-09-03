113e62c34771bae4bcd9f60de2b867cfe0b210f2
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-09-03 17:22:09 +0200

    exported new components

--- src/components/themed/index.tsx
+++ src/components/themed/index.tsx
@@ -4,3 +4,5 @@ export { default as Container } from "./Container";
 export { default as Typography } from "./Typography";
 export { default as IconButton } from "./IconButton";
 export { default as Main } from "./Main";
+export { default as Divider } from "./Divider";
+export { default as LinearProgress } from "./LinearProgress";
