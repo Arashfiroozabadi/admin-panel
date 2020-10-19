0a6ef2b33bfb81b0a14aded1e5c89757f2799575
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-10-19 15:20:52 +0200

    updated NavBtn color styles

--- src/components/themed/NavBtn/index.tsx
+++ src/components/themed/NavBtn/index.tsx
@@ -7,6 +7,8 @@ import { NavLink } from "react-router-dom";
 import { selectors } from "../../../features/counter";
 import palette from "../../../ui/palette";
 
+import { bgcTransition, } from "../../../constants/timing";
+
 import { StyledNavBtn } from "./styledNavBtn";
 
 
@@ -17,28 +19,34 @@ interface PropsTypes extends ButtonProps {
 }
 
 export default (props: PropsTypes) => {
+  // Props
   const { title, to, navclass } = props;
 
+  // Hooks
+
+  // Redux
   const t = useSelector(selectors.getTheme);
 
+
+  // Element Body
   return (
     <NavLink
       className={navclass}
       to={to}
       exact
       activeStyle={{
-        backgroundColor: palette.primary[t]
+        backgroundColor: palette.icon.nav.active.bgc[t],
       }}
       style={{
         borderRadius: 4,
-        transition: `background-color 500ms cubic-bezier(0.4, 0, 0.2, 1) 100ms!important`
+        transition: `${bgcTransition}`
       }}
     >
       <StyledNavBtn
         {...props}
         // variant="contained"
         style={{
-          color: "#DDE7F0",
+          color: palette.icon.nav.color[t],
         }}
       >
         {title}
--- src/components/themed/NavBtn/styledNavBtn.tsx
+++ src/components/themed/NavBtn/styledNavBtn.tsx
@@ -4,6 +4,7 @@ import { Button, ButtonProps } from "@material-ui/core";
 
 
 import { device } from "../../../constants/breakpoint";
+import { colorTransition } from "../../../constants/timing";
 
 interface NavProps extends ButtonProps {
 
@@ -21,9 +22,9 @@ const StyledNavBtn = styled((props: NavProps) => {
 })`
   &.root{
     justify-content: start;
+    transition: ${colorTransition};
   }
   width: 100%;
-  transition: background-color 500ms cubic-bezier(0.4, 0, 0.2, 1);
   text-transform: inherit;
   
   @media ${device.mobileS}{
