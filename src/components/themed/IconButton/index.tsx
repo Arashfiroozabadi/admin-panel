c4e400a71486651a6e0dc0faaa4865f650d098a7
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-10-21 10:37:40 +0200

    fixed style bug

--- src/components/themed/IconButton/index.tsx
+++ src/components/themed/IconButton/index.tsx
@@ -14,34 +14,39 @@ interface PropsType extends IconButtonProps {
 
 export default (props: PropsType) => {
   const t = useSelector(selectors.getTheme);
-  const { children, btntype, ...other } = props;
+  const { style, children, btntype, ...other } = props;
   return (
     <StyledIconButton
       style={
         btntype === "delete" ?
           {
             backgroundColor: palette.button.delete.bgc[t],
-            color: palette.button.delete.color[t]
+            color: palette.button.delete.color[t],
+            ...style
           } :
           btntype === "navMenu" ?
             {
               backgroundColor: palette.button.navMenu.bgc[t],
-              color: palette.button.delete.color[t]
+              color: palette.button.delete.color[t],
+              ...style
             } :
             btntype === "close" ?
               {
                 backgroundColor: palette.button.close.bgc[t],
                 color: palette.button.close.color[t],
-                padding: 7
+                padding: 7,
+                ...style
               } :
               btntype === "theme" ?
                 {
                   backgroundColor: palette.theme.bgc[t],
                   color: palette.theme.color[t],
+                  ...style
                 } :
                 {
                   backgroundColor: palette.icon.bgc[t],
-                  color: palette.icon.color[t]
+                  color: palette.icon.color[t],
+                  ...style
                 }
       }
       {...other}
