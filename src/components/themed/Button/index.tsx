bb3637deb956dfcb6c326081ec97ed30ce9d8336
arashfiroozabadi <arashfiroozabadii@gmail.com>
2020-10-24 15:32:23 +0200

    updated themed Button

--- src/components/themed/Button/index.tsx
+++ src/components/themed/Button/index.tsx
@@ -5,19 +5,42 @@ import { useSelector } from "react-redux";
 
 import palette from "../../../ui/palette";
 import { selectors } from "../../../features/counter";
+import { bgcTransition, colorTransition } from "../../../constants/timing";
 
 interface PropsTypes extends ButtonProps {
-
+  inInput?: boolean
 }
 
 export default styled((props: PropsTypes) => {
-  const { disabled, ...other } = props;
+  const {
+    style,
+    inInput,
+    disabled,
+    ...other
+  } = props;
   const t = useSelector(selectors.getTheme);
+
+  if (inInput) {
+    return (
+      <Button
+        style={{
+          color: disabled ? palette.disable.color[t] : palette.text[t],
+          transition: `${colorTransition} , ${bgcTransition}`,
+          ...style
+        }}
+        disabled={disabled}
+        {...other}
+      />
+    );
+  }
+
   return (
     <Button
       style={{
         color: disabled ? palette.disable.color[t] : palette.text[t],
-        backgroundColor: disabled ? palette.disable.bgc[t] : palette.secondary[t]
+        backgroundColor: disabled ? palette.disable.bgc[t] : palette.secondary[t],
+        transition: `${colorTransition} , ${bgcTransition}`,
+        ...style
       }}
       disabled={disabled}
       {...other}
